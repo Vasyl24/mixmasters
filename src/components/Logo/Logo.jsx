c552b18044bc7f0da43d3c62edda77b54950a997
AndrijKarmaza <andrij.karmaza@gmail.com>
2023-09-22 22:11:58 +0300

    Revert "Merge branch 'main' into my-drinks-page"
    This reverts commit b164e005ba5ee1bb2383756474a57c53020a87e4, reversing
    changes made to 75fb030a712de4e484d59db8fe52032f13a60d81.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { LogoGroup, Icon, Name } from './Logo.styled';
-import icons from '../../assets/sprite.svg';
+import icons from '../../assets/sprite.svg'
 
 export default function Logo() {
   return (
     <NavLink to="/home" style={{ zIndex: 11 }}>
       <LogoGroup>
         <Icon>
-          <svg>
-            <use xlinkHref={`${icons}#icon-logo`} />
-          </svg>
+         <a href="/home">
+            <svg>
+              <use xlinkHref={`${icons}#icon-logo`} />
+            </svg>
+          </a>
         </Icon>
         <Name>Drink Master</Name>
       </LogoGroup>
     </NavLink>
   );
 }
+
+
