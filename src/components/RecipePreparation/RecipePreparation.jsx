a99a7455995c1c865410995c18e57f2daef536e1
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-09-23 21:38:29 +0300

    Add styles for tablet and desktop

--- src/components/RecipePreparation/RecipePreparation.jsx
+++ src/components/RecipePreparation/RecipePreparation.jsx
@@ -1,12 +1,14 @@
 import foto from "../../assets/recipe-img.jpg";
-import { Title, Recipe, Image } from './RecipePreparation.styled';
+import { Title, Recipe, Image, ContainerRecipe } from './RecipePreparation.styled';
 
 export function RecipePreparation({ recipe }) {
     return (
       <>
         <Title>Recipe Preparation</Title>
-        <Recipe>{recipe}</Recipe>
-        <Image src={foto} alt="foto" />
+        <ContainerRecipe>
+          <Recipe>{recipe}</Recipe>
+          <Image src={foto} alt="foto" />
+        </ContainerRecipe>
       </>
     );
 }
