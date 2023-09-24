50bb4629af658363ea31800df026ddd7898670f9
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-09-24 18:20:19 +0200

    Search, filters added

--- src/pages/DrinksPage/DrinksPage.jsx
+++ src/pages/DrinksPage/DrinksPage.jsx
@@ -1,13 +1,49 @@
 import { Drinks } from 'components/Drinks/Drinks';
-import { DrinksSearh } from 'components/DrinksSearch/DrinksSearch';
+import { DrinksSearch } from 'components/DrinksSearch/DrinksSearch';
 import { PageTitle } from 'components/PageTitle/PageTitle';
+import { useState } from 'react';
+import drinks from '../../temporary/recipes.json';
 
 export const DrinksPage = () => {
+  const [filteredDrinks, setFilteredDrinks] = useState(drinks);
+
+  const handleSearch = text => {
+    const filtered = drinks.filter(drink =>
+      drink.drink.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredDrinks(filtered);
+  };
+
+  const handleCategoryChange = category => {
+    if (category === '') {
+      setFilteredDrinks(drinks);
+    } else {
+      const filtered = drinks.filter(drink => drink.category === category);
+      setFilteredDrinks(filtered);
+    }
+  };
+
+  const handleIngredientChange = ingredient => {
+    if (ingredient === '') {
+      setFilteredDrinks(drinks);
+    } else {
+      const filtered = drinks.filter(drink => {
+        const ingredients = drink.ingredients.map(item => item.title);
+        return ingredients.includes(ingredient);
+      });
+      setFilteredDrinks(filtered);
+    }
+  };
+
   return (
     <>
       <PageTitle title="Drinks" />
-      <DrinksSearh />
-      <Drinks />
+      <DrinksSearch
+        onSearch={handleSearch}
+        onCategoryChange={handleCategoryChange}
+        onIngredientChange={handleIngredientChange}
+      />
+      <Drinks drinks={filteredDrinks} />
       {/* <Paginator /> */}
     </>
   );
