6c4569febfe499010aca862542e3d2fdf89912bc
Olena1010 <ebojcenko55@gmail.com>
2023-09-27 17:38:43 +0300

    add

--- src/components/AddDrinkForm/DrinksIngredientsField/DrinksIngredientsField.styled.jsx
+++ src/components/AddDrinkForm/DrinksIngredientsField/DrinksIngredientsField.styled.jsx
@@ -145,3 +145,47 @@ export const DeleteIcon = styled.svg`
 		border: 1px solid #434d67;
 		border-radius: 50%;
 	}`;
+
+	export const SpriteIconMinus = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: rgba(243, 243, 243, 1);
+ 
+  @media {min-width: 768px} {
+   
+  }
+`;
+
+export const SpriteIconPlus = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: rgba(243, 243, 243, 1);
+ 
+  @media {min-width: 768px} {
+   
+  }
+`
+
+export const StyledButton = styled.button`
+width: 16px;
+height: 16px;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: inherit;
+border: none;
+outline: none;
+cursor: pointer;
+`
+
+export const CounterWrp = styled.div`
+width: 104px;
+padding: 10px 16px;
+display: flex;
+gap: 16px;
+align-items: center;
+background-color: transparent;
+border-radius: 200px;
+border: 1px solid rgba(243, 243, 243, 0.50);
+opacity: 0.8;
+`
