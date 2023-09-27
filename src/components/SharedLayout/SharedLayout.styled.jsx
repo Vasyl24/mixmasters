194d8b0029976dd26b1685c2d4423d9b0d148936
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-09-27 15:49:10 +0300

    Update SharedLayout.styled.jsx

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -2,19 +2,24 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   margin: 0 auto;
-  max-width: 375px;
   padding-left: 20px;
   padding-right: 20px;
   flex-grow: 1;
 
+  @media screen and (max-width: 767.98px) {
+    max-width: 375px;
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+
   @media screen and (min-width: 768px) {
-    max-width: 768px;
+    width: 768px;
     padding-left: 32px;
     padding-right: 32px;
   }
 
   @media screen and (min-width: 1440px) {
-    max-width: 1440px;
+    width: 1440px;
     padding-left: 100px;
     padding-right: 100px;
   }
