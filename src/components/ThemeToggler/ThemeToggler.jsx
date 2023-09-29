e1177b63922fbfbffb50d5591af1da3b890bc802
Elborn666 <elborn18@gmail.com>
2023-09-29 14:31:55 +0300

    toggle

---
+++ src/components/ThemeToggler/ThemeToggler.jsx
@@ -0,0 +1,11 @@
+// import { useState, useLayoutEffect } from "react";
+
+// export const useTheme = () => {
+//     const [theme, setTheme] = useState('light');
+
+//     useLayoutEffect(() => {
+//         return document.documentElement.setAttribute['data-theme']
+//     }, [theme])
+
+//     return { theme, setTheme }
+// }
