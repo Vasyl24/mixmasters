2d71b498cb5cb26e769f0b3f25a175015e7096c8
Elborn666 <elborn18@gmail.com>
2023-09-29 17:33:09 +0300

    почав робити тогл

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Header, HeaderWrapper } from './Header.styled';
 import Logo from '../../components/Logo/Logo';
+import Toggle from '../../components/ThemeToggler/ThemeToggler'
 import Navigation from './Navigation/Navigation';
 import BurgerMenu from './BurgerMenu/BurgerMenu';
 import { UserLogo } from 'components/UserLogo/UserLogo';
@@ -52,6 +53,7 @@ function AppHeader() {
         <HeaderWrapper>
           <Logo />
           {isDesktop && <Navigation />}
+          <Toggle />
           <UserLogo />
           <BurgerMenu
             toggleMenu={toggleMenu}
